926b03af4db24cf48b40ebd7918ba6441dff745d
kand1ss <bossvarichenko770@gmail.com>
2026-03-22 13:23:10 +0200

    feat(codec): implemented binary codec

--- src/transport/src/codec/bincode.rs
+++ src/transport/src/codec/bincode.rs
@@ -1,19 +1,35 @@
 use crate::codec::MessageCodec;
+use serde::Serialize;
+use serde::de::DeserializeOwned;
+use bincode::{config, serde as bincode_serde};
 use knot_core::errors::TransportError;
 
+
+const BINCODE_CONFIG: config::Configuration<
+    config::LittleEndian, 
+    config::Fixint,
+    config::Limit<4096>
+> = config::standard()
+    .with_little_endian()
+    .with_fixed_int_encoding()
+    .with_limit::<4096>();
+
+    
 pub struct BinaryCodec;
-impl MessageCodec for JsonCodec {
+impl MessageCodec for BinaryCodec {
     type Raw = Vec<u8>;
 
-    fn encode(message: &T) -> Result<Self::Raw, TransportError> {
-        bincode::serialize(message).map_err(|e| {
+    fn encode<T: Serialize>(message: &T) -> Result<Self::Raw, TransportError> {
+        bincode_serde::encode_to_vec(message, BINCODE_CONFIG).map_err(|e| {
             TransportError::SerializeError { reason: e.to_string() }
         })
     }
 
-    fn decode(raw: Self::Raw) -> Result<T, TransportError> {
-        bincode::deserialize(&raw).map_err(|e| {
+    fn decode<T: DeserializeOwned>(raw: Self::Raw) -> Result<T, TransportError> {
+        let (decoded, _): (T, usize) = bincode_serde::decode_from_slice(&raw, BINCODE_CONFIG).map_err(|e| {
             TransportError::DeserializeError { reason: e.to_string() }
-        })
+        })?;
+        
+        Ok(decoded)
     }
 }
